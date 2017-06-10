7f94c12179ee100c8f07f66005b2936d24a9bfb4
Collin Alpert <collinalpert@gmail.com>
2017-06-10 19:27:38 +0200

    Neue Quelle: The Guardian

--- src/DatenParty.java
+++ src/DatenParty.java
@@ -1,13 +1,21 @@
-import org.json.simple.*;
-import org.jsoup.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 @SuppressWarnings("unchecked")
 public class DatenParty {
@@ -47,6 +55,10 @@ public class DatenParty {
         add("/aktuell/frankfurter");
         add("/aktuell/stil");
         add("/aktuell/beruf-chance");
+        //Guardian
+        add("https://www.theguardian.com/world");
+        add("https://www.theguardian.com/travel");
+        add("https://www.theguardian.com/artanddesign");
     }};
 
      /** holt sich alle Artikel von der Website der Zeit*/
@@ -63,7 +75,7 @@ public class DatenParty {
                 String imglink = d.select(".article__media-item").attr("src");
                 if (!text.equals("")) values.add(new ArrayList<>(Arrays.asList(generateID(e, 3), text,
                         (t.getHour() < 10 ? "0" + t.getHour() : t.getHour()) + ":" + (t.getMinute() < 10 ? "0" + t.getMinute() : t.getMinute()), e, category, heading, imglink)));
-            } catch (IOException | ArrayIndexOutOfBoundsException e2) {
+            } catch (IOException | IndexOutOfBoundsException e2) {
                 System.out.println(e);
                 e2.printStackTrace();
             }
@@ -139,41 +151,75 @@ public class DatenParty {
         }
         return toJSON(values, "FAZ");
     }
+
+    private static ArrayList<JSONObject> getGuardian() {
+        ArrayList<ArrayList<String>> values = new ArrayList<>();
+        try {
+            ArrayList<String> links = getLinks("https://www.theguardian.com/international", ".u-faux-block-link__overlay", false);
+            for (String e: links) {
+                try {
+                    Document d = Jsoup.connect(e).get();
+                    String text = d.select(".content__article-body").get(0).getElementsByTag("p").get(0).text();
+                    String time = d.select(".content__dateline-time").get(0).text().split(" ")[0];
+                    String category = d.select(".content__section-label__link").text();
+                    String heading = d.select(".content__headline").text();
+                    String imglink = d.select(".responsive-img").attr("src");
+                    if (!text.equals(""))
+                        values.add(new ArrayList<>(Arrays.asList(generateID(e, 4), text, time, e, category, heading, imglink)));
+                } catch (Exception e2) {
+                    System.out.println(e);
+                    e2.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return toJSON(values, "The Guardian");
+    }
+
     /** hier holt sich das Programm die Links der verschiedenen Artikel */
     private static ArrayList<String> getLinks(String site, String cssQuery, boolean rename) throws Exception {
         Document doc = Jsoup.connect(site).get();
         Elements ele = doc.select(cssQuery);
         ArrayList<String> list = new ArrayList<>();
-        ele.forEach(e -> list.add(e.getElementsByAttribute("href").attr("href")));
+        ArrayList<String> newList = new ArrayList<>();
+        for (Element x: ele) list.add(x.getElementsByAttribute("href").attr("href"));
         if (rename) {
             list.removeIf(e -> !e.startsWith("/") || isInList(e));
             list.replaceAll(e -> e = site + e);
-        } else list.removeIf(e -> !e.startsWith("http://www.zeit.de") || isInList(e));
+        } else {
+            for (String e: list)
+                if ((e.startsWith("http://www.zeit.de") || e.startsWith("https://www.theguardian.com")) && !isInList(e)) newList.add(e);
+            list = newList;
+        }
         Collections.shuffle(list);
         if (list.size() > 20) list.subList(20, list.size()).clear();
         return list;
     }
+
     /** Hier werden alle Daten gesammelt und in ein Json verpackt */
-    public static void main(String[] args) throws Exception {
-        Log.write("start update");
-        ArrayList<JSONObject> zeit = getZeit();
-        ArrayList<JSONObject> welt = getWelt();
-        ArrayList<JSONObject> spiegel = getSpiegel();
-        ArrayList<JSONObject> faz = getFAZ();
-        JSONArray array = new JSONArray();
-        zeit.forEach(array::add);
-        welt.forEach(array::add);
-        spiegel.forEach(array::add);
-        faz.forEach(array::add);
-        Collections.shuffle(array);
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(daten), Charset.forName("UTF-8").newEncoder()));
-        writer.write(DetectLang.getLanguage(array).toJSONString());
-        Log.write("daten.json wurde aktualisiert!");
-        Log.write("Insgesamt " + array.size() + " Artikel");
-        writer.close();
+    public static void main(String[] args) {
+        try {
+            Log.write("start update");
+            JSONArray array = new JSONArray();
+            getZeit().forEach(array::add);
+            getWelt().forEach(array::add);
+            getSpiegel().forEach(array::add);
+            getFAZ().forEach(array::add);
+            getGuardian().forEach(array::add);
+            Collections.shuffle(array);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(daten), Charset.forName("UTF-8").newEncoder()));
+            writer.write(DetectLang.getLanguage(array).toJSONString());
+            Log.write("daten.json wurde aktualisiert!");
+            Log.write("Insgesamt " + array.size() + " Artikel");
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
     /** Diese Funktion compeliert ein normales Array zu einem Json Array */
-    private static ArrayList<JSONObject> toJSON(ArrayList<ArrayList<String>> values, String author) throws Exception {
+    private static ArrayList<JSONObject> toJSON(ArrayList<ArrayList<String>> values, String author) {
         ArrayList<JSONObject> array = new ArrayList<>();
         values.forEach(l ->
             array.add(new JSONObject() {{
@@ -193,21 +239,37 @@ public class DatenParty {
     /** diese Funktion nimmt sich die Liks und erstellt eine ID */
     private static String generateID(String text, int mode) {
         int random = new Random().nextInt(1000);
-        if (mode == 1) { //Spiegel, FAZ
-            String cut = text.split("-")[text.split("-").length-1];
-            return cut.split("\\.")[0] + random;
-        } else if (mode == 2) { //Welt
-            String cut = text.split("/")[text.split("/").length-2];
-            return cut.split("e")[1] + random;
-        } else { //Zeit
-            String[] array = text.split("/");
-            String cut = array[array.length - 1];
-            String[] date = array[array.length - 2].substring(2).split("-");
-            return date[1] + date[0] +
-                    (int)cut.charAt(0) +
-                    (int)cut.charAt(1) +
-                    (int)cut.charAt(cut.length()-2) +
-                    (int)cut.charAt(cut.length()-1) + random;
+        switch (mode) {
+            //FAZ
+            case 1: {
+                String cut = text.split("-")[text.split("-").length-1];
+                return cut.split("\\.")[0] + random;
+            //Welt
+            } case 2: {
+                String cut = text.split("/")[text.split("/").length-2];
+                return cut.split("e")[1] + random;
+            //Zeit
+            } case 3: {
+                String[] array = text.split("/");
+                String cut = array[array.length - 1];
+                String[] date = array[array.length - 2].substring(2).split("-");
+                return date[1] + date[0] +
+                        (int)cut.charAt(0) +
+                        (int)cut.charAt(1) +
+                        (int)cut.charAt(cut.length()-2) +
+                        (int)cut.charAt(cut.length()-1) + random;
+            //Guardian
+            } case 4: {
+                String[] array = text.split("/");
+                String year = array[array.length - 4];
+                String monthText = array[array.length - 3];
+                String month = Integer.toString((int) monthText.charAt(0)) + ((int) monthText.charAt(1)) + ((int) monthText.charAt(2));
+                String day = array[array.length - 2];
+                String keyText = array[array.length - 1].substring(array[array.length - 1].length()-3, array[array.length - 1].length()-1);
+                String key = Integer.toString((int) keyText.charAt(0)) + ((int) keyText.charAt(1));
+                return day+month+year+key+random;
+            }
+            default: return "error in id";
         }
     }
     /**Diese Funktion iteriert über die Listen und findet heraus ob der Link sich in der Blacklist befindet */
